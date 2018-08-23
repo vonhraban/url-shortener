d57303ee86cf5101bf95a5b773b4c93492e9ba6c
Vlad von Hraban <vlad@vivec.co.uk>
2018-08-24 00:31:10 +0100

    Add tests that check happy path and unknown key

---
+++ tests/test_url_shortener.py
@@ -0,0 +1,32 @@
+import pytest
+from falcon import testing
+from url_shortener.app import init
+import json
+
+@pytest.fixture()
+def client():
+    return testing.TestClient(init())
+
+
+def test_shorten_url(client):
+    # add a url
+    original_url = 'http://google.com'
+    body = json.dumps({
+        'url': original_url
+    })
+
+    result = client.simulate_post('/', body=body)
+    assert result.status_code == 201
+    key = result.json['key']
+
+    # retrieve the url
+    result = client.simulate_get('/' + key)
+    assert result.status_code == 301
+    retrieved_url = result.json['moved']
+    assert retrieved_url == original_url
+
+
+def test_unknown_key(client):
+    result = client.simulate_get('/unknown-key')
+    assert result.status_code == 404
+    assert result.json['error'] == "url not found"
